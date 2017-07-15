7fde9f028cf34cccc2ed1c3ea43442bae55953ed
gcrowell <graham.crowell@visiercorp.com>
2017-07-15 03:43:16 -0700

    spark group by

--- src/main/scala/com/treasure/sbx/CsvToCaseClass.scala
+++ src/main/scala/com/treasure/sbx/CsvToCaseClass.scala
@@ -24,10 +24,10 @@ object DataLoader {
     val rdr = Source.fromFile(Config.test_price_file)
     val iso_date_regex = "\\d{4}-\\d{2}-\\d{2}".r
     val number_regex = "\\d+".r
-    val csv_line = "^(\\w+),(\\d{4}-\\d{2}-\\d{2})(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)(,\\d+\\.\\d+)".r
-    rdr.getLines.drop(1).map {
+    val csv_line = "^(\\w+),(\\d{4}-\\d{2}-\\d{2}),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+\\.\\d+)".r
+    rdr.getLines.map {
       case csv_line(symbol, date, open, high, low, close, volume, _*) => {
-        Some(symbol, date.split('-').mkString.toLong, open.drop(1).toDouble, high.drop(1).toDouble, low.drop(1).toDouble, close.drop(1).toDouble, volume.drop(1).toDouble.round)
+        Some(symbol, date.split('-').mkString.toLong, open.toDouble, high.toDouble, low.toDouble, close.toDouble, volume.toDouble.round)
       }
       case _ => None
     }.toSeq
@@ -54,6 +54,8 @@ object DemoCsvConversion extends App {
     val seqPricePoint = flatData.map(TupleToPricePoint.convert)
     println(seqPricePoint)
     seqPricePoint.foreach(println)
+
+
 
   }
 }
---
+++ src/main/scala/com/treasure/sbx/spark_group_by.scala
@@ -0,0 +1,14 @@
+package com.treasure.sbx
+
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+/**
+  * Created by gcrowell on 2017-07-15.
+  */
+object spark_group_by {
+
+
+  def groupByCount(ss: SparkSession, priceDs: Dataset[PricePoint]): Int = {
+    priceDs.map((x:PricePoint)=> (x.subjectId)).distinct().count()
+  }
+}
