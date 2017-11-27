b47278ec836f26f955f4afd41abf293738e9abd7
gcrowell <graham.crowell@gmail.com>
2017-11-26 17:16:33 -0800

    unknown changes

---
+++ src/main/scala/com/treasure/data/model/Portfolio.scala
@@ -0,0 +1,15 @@
+package com.treasure.data.model
+
+trait Holding {
+  def quanity: Int
+
+}
+
+trait Holdings {
+  def cash: Double
+  def startDate: Long
+  def endDate: Long
+}
+trait Portfolio {
+
+}
